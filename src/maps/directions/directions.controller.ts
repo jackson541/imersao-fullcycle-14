6d13a4478a836afcff26616b7f7298443b662201
jackson541 <jacksonalves541@gmail.com>
2023-08-24 21:04:40 -0300

    primeira aula

---
+++ src/maps/directions/directions.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Query, Get } from '@nestjs/common';
+import { DirectionsService } from './directions.service';
+
+@Controller('directions')
+export class DirectionsController {
+    constructor(private directionsService: DirectionsService) {}
+
+    @Get()
+    getDirections(
+        @Query('originId') originId: string,
+        @Query('destinationId') destinationId: string,
+    ) {
+        return this.directionsService.getDirections(originId, destinationId);
+    }
+
+}
